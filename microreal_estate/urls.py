8bbf420ec4b266f32665080a736b214ef90f37e3
singla-arshit <singlaarshit@gmail.com>
2025-06-13 13:41:48 +0530

    dark theme of original

--- microreal_estate/urls.py
+++ microreal_estate/urls.py
@@ -23,6 +23,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
     path('accounts/', include('accounts.urls')),
     path('properties/', include('properties.urls')),
     path('payments/', include('payments.urls')),
